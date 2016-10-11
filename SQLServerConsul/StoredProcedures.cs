3c5a517bd57bbdedc2323edbcc8efcfb9f653ec6
Cihat Genc <cihat@catwithboots.com>
2016-10-11 11:34:23 +0200

    Initial solution

---
+++ SQLServerConsul/StoredProcedures.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+using Consul;
+
+namespace SQLServerConsul
+{
+    public class StoredProcedures
+    {
+        public static void UpdateConsulServices()
+        {
+            // Consul Environment --> Input from SQL Server
+            // List of Active Databases --> Input from SQL Server
+            // List of Current consul services --> GetCurrentServices()
+            // DB is leading, remove services not in active database list
+            // Register services which are not registered yet
+
+            using (var client = new ConsulClient())
+            {
+                
+            }
+        }
+
+        // Get the current services in consul
+        private static List<string> GetCurrentServices()
+        {
+            List<string> currentServices = new List<string>();
+
+            // Get the current services magic
+
+            return currentServices; 
+        }
+    }
+}
